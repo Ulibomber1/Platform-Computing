0a6574af2aa95d1355944f70fdfcc6aa866f3434
Ulibomber1 <31135793+Ulibomber1@users.noreply.github.com>
2024-03-11 10:04:38 -0700

    Setup Pass 1
    -added picture of Cleo
    -changed page name
    -replaced template content with desired 'about me' content.

--- project-1-react/src/App.js
+++ project-1-react/src/App.js
@@ -5,18 +5,36 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+      Arius Ulibarri
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello! Welcome to my about me page. My name is Arius, a current computer systems major with a minor in computer science on 
+          his last semester of college (for now). You can look at my github profile 
+          <a className="App-Link" href="https://github.com/Ulibomber1" target="_blank" rel="noopener noreferrer"> here.</a>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+        <p>
+          I have been programming for around 8 years now, with experience in several languages. First starting out I worked with BlitzMax BASIC, 
+          but afterwards I slowly transitioned to using C and C++ in Visual Studio. I programmed primarily labs for my classes, and a few 
+          Project Euler programming exercises for practice. Once I transferred here to CSUSB, I had started using Unity to develop video games 
+          which required me to learn C# and the Unity scripting API. I have a few games I had a part in creating on Itch.io: MechroKnight and 
+          Crystalpunk. I most recently learned to use the Godot game engine as well, which can also use C# as a scripting language. I have no 
+          games made in the engine yet, but I am slowly learning how to use it.
+        </p>
+        <p>
+          I am set to graduate soon, so I am currently looking for career opportunities in the software engineering and game programming fields. 
+          At the same time, once I have graduated I will also be working on my portfolio of programming projects and compiling them together on 
+          my own website and on LinkedIn, Discord, etc. 
+        </p>
+        <ul>
+          <li>Video & Tabletop Games</li>
+          <li>Bike rides</li>
+          <li>Gunpla & mecha!!</li>
+          <li>Anime & anime-adjacent media</li>
+          <li>Anything computer programming-related</li>
+          <li>My cat, Cleo (down below)</li>
+        </ul>
+        <img src="IMG_4442.jpeg" alt="Cleo"></img>
       </header>
     </div>
   );
