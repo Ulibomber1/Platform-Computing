48696ee8cace446c4964f92ff2264ead95fb5f68
Ulibomber1 <31135793+Ulibomber1@users.noreply.github.com>
2024-04-02 09:20:18 -0700

    Set up About Me Webpage as React App
    -Changed styling
    -Changed layout

--- Assignment_2_User_Interactions/project-1-react/src/App.js
+++ Assignment_2_User_Interactions/project-1-react/src/App.js
@@ -2,9 +2,6 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-
-
-
   return (
     <div className="App">
       <header className="App-header">
@@ -16,33 +13,33 @@ function App() {
           <a className="App-Link" href="https://github.com/Ulibomber1" target="_blank" rel="noopener noreferrer">here.</a>
         </p>
         
-        <h3>What I've Done</h3>
-        <div class="columnDiv">
-          <div class="contentDiv">
+        <div class="flexBoxCont">
+
+          <div class="columnDiv1">
+            <h3>What I've Done</h3>
             <p>
-              I have been programming for around 8 years now, with experience in several languages. First starting out I worked with BlitzMax BASIC, 
-              but afterwards I slowly transitioned to using C and C++ in Visual Studio. I programmed primarily labs for my classes, and a few 
-              Project Euler programming exercises for practice. Once I transferred here to CSUSB, I had started using Unity to develop video games 
-              which required me to learn C# and the Unity scripting API. I have a few games I had a part in creating on Itch.io: MechroKnight and 
-              Crystalpunk. I most recently learned to use the Godot game engine as well, which can also use C# as a scripting language. I have no 
-              games made in the engine yet, but I am slowly learning how to use it.
+            I have been programming for around 8 years now, with experience in several languages. First starting out I worked with BlitzMax BASIC, 
+            but afterwards I slowly transitioned to using C and C++ in Visual Studio. I programmed primarily labs for my classes, and a few 
+            Project Euler programming exercises for practice. Once I transferred here to CSUSB, I had started using Unity to develop video games 
+            which required me to learn C# and the Unity scripting API. I have a few games I had a part in creating on Itch.io: MechroKnight and 
+            Crystalpunk. I most recently learned to use the Godot game engine as well, which can also use C# as a scripting language. I have no 
+            games made in the engine yet, but I am slowly learning how to use it.
             </p>
           </div>
+
+        
+        <div class="columnDiv2">
+          <h3>What I'm Doing Next</h3>
+          <p>
+            I am set to graduate soon, so I am currently looking for career opportunities in the software engineering and game programming fields. 
+            At the same time, once I have graduated I will also be working on my portfolio of programming projects and compiling them together on 
+            my own website and on LinkedIn, Discord, etc. 
+          </p>
         </div>
 
-        <h3>What I'm Doing Next</h3>
-        <div class="columnDiv">
-          <div class="contentDiv">
-            <p>
-              I am set to graduate soon, so I am currently looking for career opportunities in the software engineering and game programming fields. 
-              At the same time, once I have graduated I will also be working on my portfolio of programming projects and compiling them together on 
-              my own website and on LinkedIn, Discord, etc. 
-            </p>
-          </div>
-        </div>
-
-        <h3>Hobbies & Interests</h3>
-        <div class="columnDiv">
+        
+        <div class="columnDiv3">
+          <h3>Hobbies & Interests</h3>
           <div class="listDiv">
             <ul>
               <li>Video & Tabletop Games</li>
@@ -54,6 +51,8 @@ function App() {
             </ul>
             <img class="Cleo" src="IMG_4442.jpeg" alt="Cleo"></img>
           </div>
+        </div>
+          
         </div>
       </header>
     </div>
