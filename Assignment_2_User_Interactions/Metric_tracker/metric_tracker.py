49dff32b7e998d3f2b520f31d4e709fecf32ddc3
Ulibomber1 <31135793+Ulibomber1@users.noreply.github.com>
2024-04-06 18:57:17 -0700

    Finished Metric tracker??
    -honestly don't know how to track user metrics if we didn't have users to start with
    -also, selenium is used to mimic user interactions, not webscraping or metric tracking

--- Assignment_2_User_Interactions/Metric_tracker/metric_tracker.py
+++ Assignment_2_User_Interactions/Metric_tracker/metric_tracker.py
@@ -1,3 +1,4 @@
+import pymysql.cursors
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
@@ -6,13 +7,7 @@ import random
 import time
 
 #connect to localhost database
-dbConn = pymysql.connect(
-    host='localhost:3306',
-    user='root',
-    password='1208Ilikemysql!!',
-    database='schema2',
-    charset='utf8mb4',
-    cursorclass=pymysql.cursors.DictCursor)
+dbConn = pymysql.connect(host='localhost', port=3306, user='root', password='1208Ilikemysql!!', database='schema2', charset='utf8mb4', cursorclass=pymysql.cursors.DictCursor)
 
 
 # random user variables
@@ -21,6 +16,8 @@ timeOnGithub = .05 * random.randrange(10, 150)
 
 options = Options()
 options.add_experimental_option("detach", True)
+options.add_argument("--ignore-certificate-error")
+options.add_argument("--ignore-ssl-errors")
 driver = webdriver.Chrome(options=options)
 driver.implicitly_wait(0.5)
 driver.set_page_load_timeout(20)
@@ -37,20 +34,21 @@ print("Time on site: " + str(elapsedTime))
 
 link = driver.find_element(by = By.CLASS_NAME , value="App-Link")
 linkURL = link.get_attribute('href')
-driver.get(linkURL)
-print("Github page Title: " + driver.title + "\nGithub page URL: " + linkURL)
+driver.get(str(linkURL))
+print("Github page Title: " +str(driver.title) + "\nGithub page URL: " + str(linkURL))
 time.sleep(timeOnGithub)
 
 endTime = time.time()
 elapsedTime = endTime - startTime
 print("Time on Github: " + str(elapsedTime) + "\n\n")
 
-
+dbConn.begin()
 try:
     with dbConn.cursor() as cursor:
-        sql = "INSERT INTO 'user' ('id', 'browser', 'clicked_github_link', 'time_on_about_me') VALUES (%s, %s, %s, %s)"
-        cursor.execute(sql, ())
+        sql = "INSERT INTO user (id, browser, clicked_github_link, time_on_about_me) VALUES (%s, %s, %s, %s)"
+        cursor.execute(sql, (1, driver.name, 1, elapsedTime))
     dbConn.commit()
     print("Row added successfully!")
 finally:
     dbConn.close()
+    
